67f71fc23e44c3bd1ee34091db323cc73f312859
Joshua Peter Bonzet <221312536@mycput.ac.za>
2026-02-25 09:43:44 +0200

    Added capturing code to gain student information.

--- src/main/java/Service/Main.java
+++ src/main/java/Service/Main.java
@@ -6,9 +6,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner kbd = new Scanner(System.in);
-        System.out.println("Which student are you(UngraduateStudent/Graduate): " +
-                kbd.nextLine());
+        System.out.println("Which student are you(UngraduateStudent/Graduate): ");
+        studentType = kbd.nextLine();
+        if (studentType.equalsIgnoreCase("undergraduate")) {
+            System.out.println("Enter Student ID: ");
+            String studentId = kbd.nextLine();
+            System.out.println("Enter name: ");
+            String name = kbd.nextLine();
+            System.out.println("Enter email: ");
+            String email = kbd.nextLine();
+            System.out.println("Enter department: ");
+            String department = kbd.nextLine();
+            System.out.println("Enter credit hours: ");
+            String creditHours = kbd.nextLine();
+            System.out.println("Enter scholarship amount: ");
+            double scholarshipAmount = Double.parseDouble(kbd.nextLine());
 
+            //Continue the code from here
+        }
 
 
         /*UndergraduateStudent undergrad = new UndergraduateStudent.Builder()

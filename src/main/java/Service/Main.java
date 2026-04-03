2686f8361cc29d29d1682b28cb87f27d5086a88c
Joshua Peter Bonzet <221312536@mycput.ac.za>
2026-04-03 21:27:22 +0200

    Clean up Main.java by removing commented code
    Removed commented-out code for student objects and calculations.

--- src/main/java/Service/Main.java
+++ src/main/java/Service/Main.java
@@ -21,31 +21,9 @@ public class Main {
             String creditHours = kbd.nextLine();
             System.out.println("Enter scholarship amount: ");
             double scholarshipAmount = Double.parseDouble(kbd.nextLine());
-
-            //Continue the code from here
         }
 
 
-        /*UndergraduateStudent undergrad = new UndergraduateStudent.Builder()
-                .setStudentId("221312536")
-                .setName("Joshua Bonzet")
-                .setEmail("221312536@mycput.ac.za")
-                .setDepartment("Informatics and Design")
-                .setCreditHours("5000 hours")
-                .setScholarshipAmount(50000)
-                .build();
-        System.out.println("Your tuition fee is: R" + undergrad.calculateTuition());
-        System.out.println("You are an " + undergrad.getStudentType());
-
-        GraduateStudent grad = new GraduateStudent.Builder()
-                .setStudentId("123456789")
-                .setName("Caylin Vermeulen")
-                .setEmail("caylinVermeulen@gmail.com")
-                .setDepartment("Arts and Sciences")
-                .setResearchAssistant(true)
-                .setStipend(5000)
-                .build();
-        System.out.println("You are a " + grad.getStudentType());
-        System.out.println("You tuition fee is: R" + grad.calculateTuition()); */
+        
     }
 }
